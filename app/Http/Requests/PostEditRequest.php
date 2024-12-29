f4f87711be5f93fb34b60a9e2c9b864446f37d4e
阿部莉奈 <rina7462abe@gmail.com>
2024-12-29 14:37:59 +0900

    no message

---
+++ app/Http/Requests/PostEditRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostEditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'post_title' => 'required|max:100|string',
+            'post_body' => 'required|max:5000|string',
+        ];
+    }
+
+    public function messages(){
+        return [
+            'post_title.required' => 'タイトルは入力必須です。',
+            'post_title.max' => 'タイトルは100文字以内で入力してください。',
+            'post_body.required' => '投稿内容は入力必須です。',
+            'post_body.max' => '最大文字数は5000文字です。',
+        ];
+    }
+}
